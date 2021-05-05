d406c691ec8ddd76ffb2beffb5440d9897449e35
Pechenka472 <andreymorozov007@gmail.com>
2021-05-05 12:10:03 +0500

    Password Complexity

---
+++ MegaClicker/Assets/Scripts/PasswordComplexity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class PasswordComplexity : MonoSingleton<PasswordComplexity>
+{
+    public enum Complexity
+    {
+        light,
+        weak,
+        medium,
+        reliable
+    }
+
+    public Complexity GetComplexity(string password)
+	{
+		var complexity = 0;
+
+        if (password.Length > 12) complexity++;
+        if (ContainsDigit(password)) complexity++;
+        if (ContainsLowerLetter(password)) complexity++;
+        if (ContainsUpperLetter(password)) complexity++;
+        if (ContainsSpecialCharacters(password)) complexity++;
+
+    }
+
+    private static bool ContainsLowerLetter(string password)
+    {
+        foreach (char symbol in password)
+        {
+            if ((Char.IsLetter(symbol)) && (Char.IsLower(symbol)))
+                return true;
+        }
+        return false;
+    }
+
+    private static bool ContainsUpperLetter(string pass)
+    {
+        foreach (char symbol in pass)
+        {
+            if ((Char.IsLetter(symbol)) && (Char.IsUpper(symbol)))
+                return true;
+        }
+        return false;
+    }
+
+    private static bool ContainsDigit(string pass)
+    {
+        foreach (char symbol in pass)
+        {
+            if (Char.IsDigit(symbol))
+                return true;
+        }
+        return false;
+    }
+
+    private static bool ContainsSpecialCharacters(string pass)
+    {
+        var specialCharacters = new char[] 
+        { '!', '@', '#', '$', '%', '^', '&', '*', 
+          '(', ')', '-', '_', '+', '=', ';', ':', 
+          ',', '.', '/', '?', '\', '|', '`', '~', 
+          '[', ']', '{', '}', '.' 
+        };
+
+        if (pass.Contains(specialCharacters))
+            return true;
+
+        return false;
+    }
+}
